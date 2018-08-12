9b60fd9aa91765f339740b349757c2b9346c48e9
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-08-12 20:22:25 +0600

    add: dictionaries
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

---
+++ examples/dictionaries.py
@@ -0,0 +1,10 @@
+MLB_TEAM = dict(
+    ('Colorado1', 'Rockies'),
+    ('Colorado2', 'Rockies'),
+    ('Colorado3', 'Rockies'),
+    ('Colorado4', 'Rockies'),
+    ('Colorado5', 'Rockies'),
+)
+MLB_TEAM.get('Colorado1', 'default_value')
+MLB_TEAM.pop('Colorado2')
+MLB_TEAM.clear()
