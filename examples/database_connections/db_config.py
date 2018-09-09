3829b5684d77adc4ed3b3b0d2a5438b32b69e056
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-09-10 00:43:34 +0600

    database connection pattern

--- examples/database_connections/db_config.py
+++ examples/database_connections/db_config.py
@@ -1,13 +1,40 @@
 import examples.database_connections.config as config
 
 
-def get_mysql_connection():
-    db = ""
-    return db
+class DB:
+    __connection = None
+    __cursor = None
 
+    def __init__(self, database):
+        self.__connection = self.get_connection(database)
+        self.__cursor = self.__connection.cursor()
 
-def get_database_connection(database_name):
-    return {
-        config.MYSQL: get_mysql_connection()
-    }[database_name]
+    def get_connection(self, database):
+        __db_config = config.CONFIG[database]
+        __db = None
 
+        if database == config.MYSQL:
+            try:
+                import mysql.connector
+                from mysql.connector import errorcode
+
+                try:
+                    __db = mysql.connector.connect(**__db_config)
+                except mysql.connector.Error as err:
+                    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                        print("Something is wrong with your user name or password")
+                    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                        print("Database does not exist")
+                    else:
+                        print("Problem in Database Connection")
+            except Exception as e:
+                print(f'Could not import the {database} package.\n{e}')
+
+        return __db
+
+    def query(self, query, params):
+        self.__cursor.execute(query, params)
+        return self.__cursor
+
+    def close(self):
+        self.__connection.close()
