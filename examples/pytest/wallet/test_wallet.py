5ec7c243e6b332fdb17c0f63d247150bcbda8fdc
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-04-13 12:20:19 +0600

    test-wallet

---
+++ examples/pytest/wallet/test_wallet.py
@@ -0,0 +1,37 @@
+import pytest
+from .wallet import Wallet, InsufficientAmount
+
+
+@pytest.fixture
+def empty_wallet():
+    '''Returns a wallet instance with zero balance'''
+    return Wallet()
+
+
+@pytest.fixture
+def wallet():
+    '''Returns a wallet instance with balance == 20'''
+    return Wallet(20)
+
+
+def test_default_initial_amount(empty_wallet):
+    assert empty_wallet.balance == 0
+
+
+def test_initial_amount(wallet):
+    assert wallet.balance == 20
+
+
+def test_add_cash(wallet):
+    wallet.add_cash(10)
+    assert wallet.balance == 30
+
+
+def test_spend_cash(wallet):
+    wallet.spend_cash(10)
+    assert wallet.balance == 10
+    
+
+def test_wallet_spend_cash_raises_exception_on_insufficient_amount(wallet):
+    with pytest.raises(InsufficientAmount):
+        wallet.spend_cash(100)
