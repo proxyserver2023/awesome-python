b9891d43a8d42332a2d58fa1048d9e41d2c8bed5
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-04-13 12:10:30 +0600

    exception handling test

--- examples/pytest/test_capital_case.py
+++ examples/pytest/test_capital_case.py
@@ -1,3 +1,10 @@
+import pytest
 from capital_case import *
+
+
 def test_capital_case():
     assert capital_case('alamin') == 'Alamin'
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        capital_case(9)
